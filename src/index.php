b88f845745b3d6e49b15446b128ea782df33bf31
Kaitlyn Callow <kaitlyn@exacttechnology.com>
2024-07-27 23:44:14 -0400

    added language tags to history

--- src/index.php
+++ src/index.php
@@ -22,25 +22,29 @@
 			<h2>Software Developer — Team Lead — Systems Architect — DevOps</h2>
 		</div>
 		
-		<div class="profile">
-			<img src="assets/coders2.png" alt="Kaitlyn in action!" />
+		<div class="intro">
+			<div class="profile">
+				<img src="assets/coders2.png" alt="Kaitlyn in action!" />
+				<div>
+					<span>Kaitlyn Callow</span>
+					<span>Coder Extraordinaire</span>
+					<span>Powered by Coffee & Spite</span>
+					<span>Located in Toronto Canada <img src="assets/canada2.png" alt="Flag of Canada" style="height: 11px; margin-left: 5px; margin-bottom: -1px;" /></span>
+				</div>
+			</div>
+			
 			<div>
-				<span>Kaitlyn Callow</span>
-				<span>Coder Extraordinaire</span>
-				<span>Powered by coffee & spite</span>
-				<span>Located in Toronto Canada <img src="assets/canada2.png" alt="Flag of Canada" style="height: 11px; margin-left: 5px; margin-bottom: -1px;" /></span>
+				<h3>What I do</h3>
+				<p>I help companies grow and thrive.  I have more than 15 years experience developing software products from prototype to production and beyond.</p>
+				<div class="socials">
+					<a href="https://www.linkedin.com/in/kaitlyncallow/"><img alt="LinkedIn" src="assets/LI-Logo.png" /></a>
+				</div>
+
+				<p class="languages omnom">
+					Food for search engines: Programming, Databases, Systems Design, DevOps, AWS, PHP, MySQL, Mobile, Cloud, Distributed, UX, UI, Design, Front-End, Back-End, Apache, Nginx, Linux, OpenGL, WebGL, Jira, Git, Slack, C#, Java, MSSQL, C++, Python, SQL, HTML, CSS, JavaScript, jQuery, React.js, Layout, Planning, Prototyping, ElasticBeanstalk, EC2, RDS, Amplify, Route53, Lamba, IoT, LwM2M, Particle.io, Android, iOS
+				</p>
 			</div>
 		</div>
-		
-		<h3>What I do</h3>
-		<p>I help companies grow and thrive.  I have more than 15 years experience developing software products from prototype to production and beyond.</p>
-		<div class="socials">
-			<a href="https://www.linkedin.com/in/kaitlyncallow/"><img alt="LinkedIn" src="assets/LI-Logo.png" /></a>
-		</div>
-
-		<p class="languages omnom">
-			Food for search engines: Programming, Databases, Systems Design, DevOps, AWS, PHP, MySQL, Mobile, Cloud, Distributed, UX, UI, Design, Front-End, Back-End, Apache, Nginx, Linux, OpenGL, WebGL, Jira, Git, Slack, C#, Java, MSSQL, C++, Python, SQL, HTML, CSS, JavaScript, jQuery, React.js, Layout, Planning, Prototyping, ElasticBeanstalk, EC2, RDS, Amplify, Route53, Lamba, IoT, LwM2M, Particle.io, Android, iOS
-		</p>
 		
 		<h3>My Work</h3>
 		
@@ -50,7 +54,20 @@
 					<h4>EXACT Technology</h4>
 					<div>Software Tech Lead (formerly Track Product Lead)</div>
 					<div>2020 &mdash; Now</div>
-					<div>Concrete monitoring platform.  Real-time data services, hardware products, and suites of tools for managing concrete. Track focused on digital tools that capture all aspects of planning, qc, and scheduling.  After 2 years I switched to a Tech Lead position overseeing all software teams.</div>
+					<p>Concrete monitoring platform.  Real-time data services, hardware products, and suites of tools for managing concrete. Track focused on digital tools that capture all aspects of planning, qc, and scheduling.  After 2 years I switched to a Tech Lead position overseeing all software teams.</p>
+					<ul><li>
+						AWS
+					</li><li>
+						PHP
+					</li><li>
+						MySQL
+					</li><li>
+						Python
+					</li><li>
+						React
+					</li><li>
+						React Native
+					</li></ul>
 				</div>
 				<div class="company-image"><img src="assets/projects/exact.png" alt="EXACT Technology" /></div>
 			</div>
@@ -59,7 +76,18 @@
 					<h4>Cevnn Payments</h4>
 					<div>Lead Software Engineer</div>
 					<div>2019</div>
-					<div>Payment gateway service with integrations for commonly used checkout carts.  I was hired to move the product beyond the prototype stage and get the team established.  We had significant growth in the year I was working there.</div>
+					<p>Payment gateway service with integrations for commonly used checkout carts.  I was hired to move the product beyond the prototype stage and get the team established.  We had significant growth in the year I was working there.</p>
+					<ul><li>
+						AWS
+					</li><li>
+						PHP
+					</li><li>
+						MySQL
+					</li><li>
+						React
+					</li><li>
+						NodeJS
+					</li></ul>
 				</div>
 				<div class="company-image"><img src="assets/projects/cevnn.png" alt="Cevnn Payments" /></div>
 			</div>
@@ -68,7 +96,14 @@
 					<h4>Clausehound</h4>
 					<div>Senior Full-Stack Developer</div>
 					<div>2018</div>
-					<div>Law tools and services for collaboration, knowledge capture, organization, and document assembly.</div>
+					<p>Law tools and services for collaboration, knowledge capture, organization, and document assembly.</p>
+					<ul><li>
+						PHP
+					</li><li>
+						MySQL
+					</li><li>
+						React
+					</li></ul>
 				</div>
 				<div class="company-image"><img src="assets/projects/Clausehound.jpeg" alt="Clausehound" /></div>
 			</div>
@@ -77,7 +112,26 @@
 					<h4>Bitcurve Systems</h4>
 					<div>Full-Stack Developer</div>
 					<div>2012 &mdash; 2017</div>
-					<div>Software agency, digital design & development studio.  Wide range of clients. Largly medical industry projects as well as work for government agencies.  Planned and produced more then 35 software products from concept to production.</div>
+					<p>Software agency, digital design & development studio.  Wide range of clients. Largly medical industry projects as well as work for government agencies.  Planned and produced more then 35 software products from concept to production.</p>
+					<ul><li>
+						Linux
+					</li><li>
+						PHP
+					</li><li>
+						MySQL
+					</li><li>
+						JavaScript
+					</li><li>
+						jQuery
+					</li><li>
+						Java
+					</li><li>
+						C#
+					</li><li>
+						PhoneGap
+					</li><li>
+						WebGL
+					</li></ul>
 				</div>
 				<div class="company-image"><img src="assets/projects/Bitcurve.png" alt="Bitcurve" /></div>
 			</div>
@@ -86,7 +140,20 @@
 					<h4>Seneca College</h4>
 					<div>Software Developer</div>
 					<div>2011</div>
-					<div>Developed a blackberry application for the Center for Development of Open Technology.  Interactive training materials prepared for the Center for Academic Excellence.</div>
+					<p>Developed a blackberry application for the Center for Development of Open Technology.  Interactive training materials prepared for the Center for Academic Excellence.</p>
+					<ul><li>
+						BlackBerry
+					</li><li>
+						Java
+					</li><li>
+						Photoshop
+					</li><li>
+						Processing
+					</li><li>
+						JavaScript
+					</li><li>
+						PhoneGap
+					</li></ul>
 				</div>
 				<div class="company-image"><img src="assets/projects/Seneca.png" alt="Seneca College" /></div>
 			</div>
@@ -95,7 +162,22 @@
 					<h4>Webcom</h4>
 					<div>Software Developer</div>
 					<div>2009</div>
-					<div>Book printing company, worked in I.T. migrating data and code from old systems to newer web technologies and languages.</div>
+					<p>Book printing company, worked in I.T. migrating data and code from old systems to newer web technologies and languages.</p>
+					<ul><li>
+						MS SQL
+					</li><li>
+						MySQL
+					</li><li>
+						PHP
+					</li><li>
+						ASP.net
+					</li><li>
+						MS Access
+					</li><li>
+						Excel
+					</li><li>
+						VBScript
+					</li></ul>
 				</div>
 				<div class="company-image"><img src="assets/projects/Webcom.png" alt="Webcom" /></div>
 			</div>
@@ -104,7 +186,10 @@
 					<h4>Lambton College</h4>
 					<div>Software Developer</div>
 					<div>2007</div>
-					<div>Prepared internal tools for administration as part of the I.T. team.</div>
+					<p>Prepared internal tools for administration as part of the I.T. team.</p>
+					<ul><li>
+						PHP
+					</li></ul>
 				</div>
 				<div class="company-image"><img src="assets/projects/Lambton.jpg" alt="Lambton College" /></div>
 			</div>
